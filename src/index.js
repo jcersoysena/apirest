a46aa5707dc156ad407e725a4017395643a18085
JUAN CARLOS ESCOBAR RODRIGUEZ <jcer@soy.sena.edu.co>
2023-04-24 20:30:46 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+app.set('port', process.env.PORT || 4000);
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+app.use(require('./routes/index'));
+
+app.use('/api/products', require('./routes/products'));
+
+app.listen(app.get('port'), () => {
+    console.log('Server on port ${app.get("port")}');
+});
---
+++ src/routes/products.js
@@ -0,0 +1,22 @@
+const {Router} = require('express');
+
+const router = new Router();
+const metodito = require('underscore');
+
+const productos = require('../bdproducts.json');
+
+router.get('/', (req, res) =>{
+    res.json(productos)
+});
+
+router.post('/', (req, res) =>{
+    const{nombreProd, cantidad, precio, estado} = req.body;
+    const id = productos.length + 1; 
+    if (nombreProd, cantidad, precio, estado) {
+        const newProduct = {...req.body, id};
+        productos.push(newProduct):
+        res.json(productos);
+       } else{
+        res.status(500).json({error: 'posible error.'});
+       }
+});
