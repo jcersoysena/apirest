d29f6c101601733e087ce5657cbc344547356c18
JUAN CARLOS ESCOBAR RODRIGUEZ <jcer@soy.sena.edu.co>
2023-04-25 22:09:21 -0500

    firs commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
-app.set('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || 3000);
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended:false}));
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(require('./routes/index'));
 
 app.use('/api/products', require('./routes/products'));
-
+//ads
 app.listen(app.get('port'), () => {
 console.log(`Server on port ${port.get('port')}`);
 });
